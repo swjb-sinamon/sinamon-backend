92944651bce45eaead137972017b952105c11e7c
SkyLightQP <combbm@naver.com>
2020-08-13 14:44:50 +0900

    feat: create Subjects table model

---
+++ src/databases/models/subjects.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../index';
+
+interface SubjectsAttributes {
+  id: number;
+  subjectName: string;
+}
+
+class Subjects extends Model<SubjectsAttributes> {
+  public id!: number;
+
+  public subjectName!: string;
+}
+
+Subjects.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    subjectName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  { sequelize, modelName: 'subjects', timestamps: false }
+);
+
+(async () => {
+  await sequelize.query('ALTER TABLE subjects AUTO_INCREMENT=100;');
+})();
+
+export default Subjects;
