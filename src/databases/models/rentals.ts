01a351b7e6cf605f7b62623c9e5e0a99f2aaabf7
SkyLightQP <combbm@naver.com>
2020-08-13 14:44:45 +0900

    feat: create Rentals table model

---
+++ src/databases/models/rentals.ts
@@ -0,0 +1,58 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../index';
+
+interface RentalsAttributes {
+  uuid: string;
+  lender: string;
+  umbrellaName: string;
+  expiryDate: Date;
+  isExpire: boolean;
+}
+
+class Rentals extends Model<RentalsAttributes> {
+  public uuid!: string;
+
+  public lender!: string;
+
+  public umbrellaName!: string;
+
+  public expiryDate!: Date;
+
+  public isExpire!: boolean;
+
+  public readonly createdAt!: Date;
+
+  public readonly updatedAt!: Date;
+}
+
+Rentals.init(
+  {
+    uuid: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true,
+      primaryKey: true
+    },
+    lender: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      unique: true
+    },
+    umbrellaName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    expiryDate: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    isExpire: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    }
+  },
+  { sequelize, modelName: 'rentals', timestamps: true }
+);
+
+export default Rentals;
