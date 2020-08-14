03d53bce585b6e1b04ec8ec4b5442f2a50af436d
SkyLightQP <combbm@naver.com>
2020-08-14 20:59:57 +0900

    feat: add SERVER_ERROR error message

--- src/error/error-message.ts
+++ src/error/error-message.ts
@@ -1,5 +1,6 @@
 enum ErrorMessage {
-  USER_NOT_FOUND = '이메일이 존재하지 않거나 비밀번호가 올바르지 않습니다.'
+  USER_NOT_FOUND = '이메일이 존재하지 않거나 비밀번호가 올바르지 않습니다.',
+  SERVER_ERROR = '오류가 발생하였습니다. 잠시후 다시 시도해주세요.'
 }
 
 export default ErrorMessage;
