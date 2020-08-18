d232d7c3eb79e4020dedd75664627a02cea63b97
SkyLightQP <combbm@naver.com>
2020-08-18 22:41:33 +0900

    feat: add NO_PERMISSION error message

--- src/error/error-message.ts
+++ src/error/error-message.ts
@@ -1,6 +1,7 @@
 enum ErrorMessage {
   USER_NOT_FOUND = '이메일이 존재하지 않거나 비밀번호가 올바르지 않습니다.',
   USER_ALREADY_EXISTS = '이미 존재하는 이메일입니다.',
+  NO_PERMISSION = '권한이 없습니다.',
   SERVER_ERROR = '오류가 발생하였습니다. 잠시후 다시 시도해주세요.'
 }
 
