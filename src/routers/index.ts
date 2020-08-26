b943bdc1541d6032c1f157583d99cc53f0db60f5
SkyLightQP <combbm@naver.com>
2020-08-26 22:01:26 +0900

    feat: implement /umbrella router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import AuthRouter from './auth-router';
 import SubjectRouter from './subject-router';
+import UmbrellaRouter from './umbrella-router';
 
 const router = express.Router();
 
 router.use('/auth', AuthRouter);
 router.use('/subject', SubjectRouter);
+router.use('/umbrella', UmbrellaRouter);
 
 export default router;
---
+++ src/routers/umbrella-router.ts
@@ -0,0 +1,144 @@
+import express from 'express';
+import { body, param } from 'express-validator';
+import { requireAuthenticated } from '../middlewares/permission';
+import {
+  createUmbrella,
+  getUmbrella,
+  getUmbrellas,
+  removeUmbrella,
+  updateUmbrella
+} from '../services/umbrella-service';
+import { logger } from '../index';
+import { makeError } from '../error/error-system';
+import ErrorMessage from '../error/error-message';
+import { checkValidation } from '../middlewares/validator';
+import UmbrellaStatus from '../types/umbrella-status';
+
+const router = express.Router();
+
+router.get('/', requireAuthenticated, async (req, res) => {
+  try {
+    const data = await getUmbrellas();
+    res.status(200).json({
+      success: true,
+      data
+    });
+    logger.info('전체 우산을 가져왔습니다.');
+  } catch (e) {
+    logger.error('전체 우산을 가져오는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+  }
+});
+
+const getUmbrellaValidator = [
+  param('name').isString()
+];
+router.get('/:name', getUmbrellaValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
+  try {
+    const { name } = req.params;
+    const data = await getUmbrella(name);
+
+    if (!data) {
+      res.status(404).json(makeError(ErrorMessage.UMBRELLA_NOT_FOUND));
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${name} 우산을 가져왔습니다.`);
+  } catch (e) {
+    logger.error('우산을 가져오는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+  }
+});
+
+const createUmbrellaValidator = [
+  body('name').isString(),
+  body('status').isIn(Object.values(UmbrellaStatus))
+];
+router.post('/', createUmbrellaValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
+  const { name, status } = req.body;
+
+  try {
+    const data = await createUmbrella(name, status);
+
+    if (!data) {
+      res.status(409).json(makeError(ErrorMessage.UMBRELLA_ALREADY_EXISTS));
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${name} 우산을 만들었습니다.`);
+  } catch (e) {
+    logger.error('우산을 추가하는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+  }
+});
+
+const updateUmbrellaValidator = [
+  param('name').isString(),
+  body('status').isIn(Object.values(UmbrellaStatus))
+];
+router.put('/:name', updateUmbrellaValidator, checkValidation, requireAuthenticated,
+  async (req: express.Request, res: express.Response) => {
+    const { name } = req.params;
+    const { status } = req.body;
+    try {
+      const data = await updateUmbrella(name, status);
+
+      if (!data) {
+        res.status(404).json(makeError(ErrorMessage.UMBRELLA_NOT_FOUND));
+        return;
+      }
+
+      res.status(200).json({
+        success: true,
+        data
+      });
+
+      logger.info(`${name} 우산을 수정했습니다.`);
+    } catch (e) {
+      logger.error('우산을 수정하는 중에 오류가 발생하였습니다.');
+      logger.error(e);
+      res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+    }
+  });
+
+const removeUmbrellaValidator = [
+  param('name').isString()
+];
+router.delete('/:name', removeUmbrellaValidator, checkValidation, requireAuthenticated,
+  async (req: express.Request, res: express.Response) => {
+    const { name } = req.params;
+    try {
+      const data = await removeUmbrella(name);
+
+      if (!data) {
+        res.status(404).json(makeError(ErrorMessage.UMBRELLA_NOT_FOUND));
+        return;
+      }
+
+      res.status(200).json({
+        success: true,
+        data
+      });
+
+      logger.info(`${name} 우산을 삭제했습니다.`);
+    } catch (e) {
+      logger.error('우산을 삭제하는 중에 오류가 발생하였습니다.');
+      logger.error(e);
+      res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+    }
+  });
+
+export default router;
