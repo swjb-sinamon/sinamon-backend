2872588035ea2fb90217e31bb7c1fe6a3a89499b
SkyLightQP <combbm@naver.com>
2020-08-26 18:48:22 +0900

    feat: add log message in subject router

--- src/routers/subject-router.ts
+++ src/routers/subject-router.ts
@@ -5,51 +5,76 @@ import { createSubject, getSubject, getSubjects, removeSubject, updateSubject } 
 import { checkValidation } from '../middlewares/validator';
 import { makeError } from '../error/error-system';
 import ErrorMessage from '../error/error-message';
+import { logger } from '../index';
 
 const router = express.Router();
 
 router.get('/', requireAuthenticated, async (req, res) => {
-  const data = await getSubjects();
-  res.status(200).json({
-    success: true,
-    data
-  });
+  try {
+    const data = await getSubjects();
+    res.status(200).json({
+      success: true,
+      data
+    });
+
+    logger.info('전체 과목을 불러왔습니다.');
+  } catch (e) {
+    logger.error('전체 과목을 불러오는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
+  }
 });
 
 const getSubjectValidator = [
   param('id').isNumeric()
 ];
 router.get('/:id', getSubjectValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const data = await getSubject(parseInt(id, 10));
+  try {
+    const { id } = req.params;
+    const data = await getSubject(parseInt(id, 10));
 
-  if (!data) {
-    res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
-    return;
+    if (!data) {
+      res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${id} 과목을 불러왔습니다.`);
+  } catch (e) {
+    logger.error('과목을 불러오는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
   }
-
-  res.status(200).json({
-    success: true,
-    data
-  });
 });
 
 const createSubjectValidator = [
   body('subjectName').isString()
 ];
 router.post('/', createSubjectValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
-  const { subjectName } = req.body;
-  const data = await createSubject(subjectName);
+  try {
+    const { subjectName } = req.body;
+    const data = await createSubject(subjectName);
 
-  if (!data) {
-    res.status(409).json(makeError(ErrorMessage.SUBJECT_ALREADY_EXISTS));
-    return;
+    if (!data) {
+      res.status(409).json(makeError(ErrorMessage.SUBJECT_ALREADY_EXISTS));
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${data.id} 새로운 과목을 추가했습니다.`);
+  } catch (e) {
+    logger.error('과목을 추가하는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
   }
-
-  res.status(201).json({
-    success: true,
-    data
-  });
 });
 
 const updateSubjectValidator = [
@@ -57,37 +82,53 @@ const updateSubjectValidator = [
   body('subjectName').isString()
 ];
 router.put('/:id', updateSubjectValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const { subjectName } = req.body;
-  const data = await updateSubject(parseInt(id, 10), subjectName);
+  try {
+    const { id } = req.params;
+    const { subjectName } = req.body;
+    const data = await updateSubject(parseInt(id, 10), subjectName);
 
-  if (!data) {
-    res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
-    return;
+    if (!data) {
+      res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${id} 과목을 수정하였습니다.`);
+  } catch (e) {
+    logger.error('과목을 수정하는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
   }
-
-  res.status(200).json({
-    success: true,
-    data
-  });
 });
 
 const removeSubjectValidator = [
   param('id').isNumeric()
 ];
 router.delete('/:id', removeSubjectValidator, checkValidation, requireAuthenticated, async (req: express.Request, res: express.Response) => {
-  const { id } = req.params;
-  const data = await removeSubject(parseInt(id, 10));
+  try {
+    const { id } = req.params;
+    const data = await removeSubject(parseInt(id, 10));
 
-  if (!data) {
-    res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
-    return;
+    if (!data) {
+      res.status(404).json(makeError(ErrorMessage.SUBJECT_NOT_FOUND));
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      data
+    });
+
+    logger.info(`${id} 과목을 삭제하였습니다.`);
+  } catch (e) {
+    logger.error('과목을 삭제하는 중에 오류가 발생하였습니다.');
+    logger.error(e);
+    res.status(500).json(makeError(ErrorMessage.SERVER_ERROR));
   }
-
-  res.status(200).json({
-    success: true,
-    data
-  });
 });
 
 export default router;
