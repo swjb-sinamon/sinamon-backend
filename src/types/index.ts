11439651d642d96b13471c2d176ae0d46d485227
SkyLightQP <combbm@naver.com>
2020-09-10 22:29:10 +0900

    fix: remove unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,13 +3,5 @@ export enum UmbrellaStatus {
   WORSE = 'worse'
 }
 
-export type NotFound = undefined;
-export type AlreadyExists = undefined;
-
 export const DayWeekArray = ['월', '화', '수', '목', '금', '토', '일'] as const;
 export type DayWeek = typeof DayWeekArray[number];
-
-export interface TupleError {
-  readonly prepareError: boolean;
-  readonly error: boolean;
-}
