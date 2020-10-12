2d6e72e1329d597dfb785847cbdbcc4bac8374f8
SkyLightQP <combbm@naver.com>
2020-10-12 12:51:37 +0900

    fix: FIx calender logic to range()

--- src/services/school/calender-service.ts
+++ src/services/school/calender-service.ts
@@ -1,18 +1,19 @@
 import dayjs from 'dayjs';
+import { range } from 'fxjs';
 import school from '../../utils/school-lib';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getThisWeekCalender = async (): Promise<string[]> => {
-  const arr = ['', '', '', '', ''];
   const calender: any = await school.getCalendar();
 
-  const result = arr.map((v, i) => {
-    const thisWeek = dayjs().date() + (i - dayjs().day());
-    const thisDay = dayjs()
-      .set('date', thisWeek)
-      .date();
-    return calender[thisDay];
-  });
+  const result = range(0, 5)
+    .map((v: string, i: number) => {
+      const thisWeek = dayjs().date() + (i - dayjs().day());
+      const thisDay = dayjs()
+        .set('date', thisWeek)
+        .date();
+      return calender[thisDay];
+    });
 
   return result;
 };
