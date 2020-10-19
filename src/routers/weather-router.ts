4b8e6e0798c355aca2f281a1ce9838c8bcd9dd51
combbm <combbm@naver.com>
2020-10-19 12:30:15 +0900

    feat: implement fine dust api

--- src/routers/weather-router.ts
+++ src/routers/weather-router.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import { getWeatherStatus } from '../services/weather-service';
+import { getDustData, getWeatherStatus } from '../services/weather-service';
 
 const router = express.Router();
 
 router.get('/', async (req: express.Request, res: express.Response) => {
   const data = await getWeatherStatus();
+  res.status(200).json({
+    success: true,
+    data
+  });
+});
+
+router.get('/dust', async (req: express.Request, res: express.Response) => {
+  const data = await getDustData();
   res.status(200).json({
     success: true,
     data
--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -1,8 +1,28 @@
 import axios from 'axios';
+import { parseStringPromise } from 'xml2js';
 import config from '../config';
 
-// eslint-disable-next-line import/prefer-default-export
+interface DustPayload {
+  readonly pm25: number;
+  readonly pm10: number;
+}
+
 export const getWeatherStatus = async (): Promise<string> => {
   const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Suwon,KR&appid=${config.openWeatherApiKey}&units=metric`);
   return result.data.weather[0].main.toUpperCase();
+};
+
+export const getDustData = async (): Promise<DustPayload> => {
+  const apiUrl = `http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?numOfRows=13&pageNo=1&sidoName=%EA%B2%BD%EA%B8%B0&searchCondition=HOUR&ServiceKey=${config.dustApiKey}`;
+  const result = await axios.get(apiUrl);
+  const xml = await parseStringPromise(result.data);
+
+  const data = xml.response.body[0].items[0].item[12];
+  const pm25 = data.pm25Value[0];
+  const pm10 = data.pm10Value[0];
+
+  return {
+    pm25,
+    pm10
+  };
 };
