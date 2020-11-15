4cbd473bd8be7c7666ba83f2917606418bff01a6
combbm <combbm@naver.com>
2020-11-15 21:36:18 +0900

    chore: apply chalk dependency

--- bin/makeActivationCode.ts
+++ bin/makeActivationCode.ts
@@ -1,7 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line import/no-extraneous-dependencies
+import chalk from 'chalk';
 import { addActivationCode } from '../src/services/activation-code-service';
 
+// 인증코드 생성용 스크립트 파일
+
 (async () => {
-  await addActivationCode();
-  console.log('인증코드 생성 완료');
+  const result = await addActivationCode();
+  console.log(chalk.green(`${chalk.underline(result.code)} 인증코드 생성 완료`));
   process.exit(0);
 })();
