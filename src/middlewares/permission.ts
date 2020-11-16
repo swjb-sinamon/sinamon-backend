c7b02eac95a271232f9ec8464087fa0ac94b9f63
combbm <combbm@naver.com>
2020-11-16 15:11:54 +0900

    feat: implement requirePermission() middleware

--- src/middlewares/permission.ts
+++ src/middlewares/permission.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { makeError } from '../error/error-system';
 import ErrorMessage from '../error/error-message';
+import { PermissionType } from '../types';
+import Permissions from '../databases/models/permissions';
+import { logger } from '../index';
 
-// eslint-disable-next-line import/prefer-default-export
 export const requireAuthenticated = (
   req: express.Request,
   res: express.Response,
@@ -14,4 +16,46 @@ export const requireAuthenticated = (
   }
 
   next();
+};
+
+export const requirePermission = (type: PermissionType[]) => (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const { user }: any = req;
+
+  if (!user) {
+    res.status(401).json(makeError(ErrorMessage.NO_PERMISSION));
+    return;
+  }
+
+  Permissions.findOne({
+    where: {
+      uuid: user.uuid
+    }
+  }).then((data) => {
+    if (!data) {
+      res.status(401).json(makeError(ErrorMessage.NO_PERMISSION));
+      return;
+    }
+
+    const myPermission: PermissionType[] = [];
+
+    if (data.isAdmin) {
+      myPermission.push('admin');
+    }
+
+    if (data.isTeacher) {
+      myPermission.push('teacher');
+    }
+
+    if (data.isSchoolUnion) {
+      myPermission.push('schoolunion');
+    }
+
+    const result = myPermission.some((v) => type.includes(v));
+    if (result) next();
+    else res.status(401).json(makeError(ErrorMessage.NO_PERMISSION));
+  });
 };
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,5 @@ export enum UmbrellaStatus {
 
 export const DayWeekArray = ['월', '화', '수', '목', '금', '토', '일'] as const;
 export type DayWeek = typeof DayWeekArray[number];
+
+export type PermissionType = 'admin' | 'teacher' | 'schoolunion';
