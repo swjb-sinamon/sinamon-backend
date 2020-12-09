555b529d900c1aac5f74149708d6a17872c30c86
combbm <combbm@naver.com>
2020-12-09 16:33:29 +0900

    fix: fix date of uniform start and end

--- src/databases/Initialize.ts
+++ src/databases/Initialize.ts
@@ -3,6 +3,7 @@ import dayjs from 'dayjs';
 import ServerConfigs from './models/server-configs';
 import { logger } from '../index';
 import Uniform from './models/uniform';
+import config from '../config';
 
 export const initializeServerConfig = async (): Promise<void> => {
   const notice = await ServerConfigs.findOne({
@@ -21,18 +22,20 @@ export const initializeServerConfig = async (): Promise<void> => {
   }
 };
 
-const TOTAL_DATA = 18 * 8;
+const UNIFORM_START_DATE = config.uniform.start;
+const UNIFORM_END_DATE = config.uniform.end;
+const UNIFORM_TOTAL_DATA = 18 * (UNIFORM_END_DATE - UNIFORM_START_DATE + 1);
 
 export const initializeUniformData = async (): Promise<void> => {
   const count = await Uniform.count();
 
-  if (count === TOTAL_DATA) return;
+  if (count === UNIFORM_TOTAL_DATA) return;
 
   const promise = range(1, 3)
     .map((i: number) => {
       return range(1, 10)
         .map((j: number) => {
-          return range(9, 17)
+          return range(UNIFORM_START_DATE, UNIFORM_END_DATE + 1)
             .map(async (k: number) => {
               const date = dayjs()
                 .set('month', 11)
