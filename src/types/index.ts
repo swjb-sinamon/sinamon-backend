f6b93e47bc69f54089351d99611de8c6ece484af
combbm <combbm@naver.com>
2020-12-22 00:45:16 +0900

    feat: add ComciganTimetable type

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,15 @@ export const DayWeekArray = ['월', '화', '수', '목', '금', '토', '일'] as
 export type DayWeek = typeof DayWeekArray[number];
 
 export type PermissionType = 'admin' | 'teacher' | 'schoolunion';
+
+export interface ComciganTimetable {
+  readonly grade: number;
+  readonly class: number;
+  readonly weekday: number;
+  readonly weekdayString: string;
+  // eslint-disable-next-line camelcase
+  readonly class_time: number;
+  readonly code: string;
+  readonly teacher: string;
+  readonly subject: string;
+}
