93580384b3f71083a8cb2116be4f4ce373034c1e
combbm <combbm@naver.com>
2020-12-22 15:51:23 +0900

    fix: remove unnecessary routers and services

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import AuthRouter from './auth-router';
-import SubjectRouter from './subject-router';
 import UmbrellaRouter from './umbrella/umbrella-router';
 import UmbrellaGetterRouter from './umbrella/umbrella-getter-router';
-import OnlineTimeTableRouter from './online-time-table-router';
 import QrRouter from './qr-router';
 import SchoolRouter from './school-router';
 import WeatherRouter from './weather-router';
@@ -14,10 +12,8 @@ import UniformRouter from './uniform-router';
 const router = express.Router();
 
 router.use('/auth', AuthRouter);
-router.use('/subject', SubjectRouter);
 router.use('/umbrella', UmbrellaRouter);
 router.use('/umbrella', UmbrellaGetterRouter);
-router.use('/ott', OnlineTimeTableRouter);
 router.use('/qr', QrRouter);
 router.use('/school', SchoolRouter);
 router.use('/weather', WeatherRouter);
