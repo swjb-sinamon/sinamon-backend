2b01150a42af798db40f913e5b162bdf3dfb15e7
combbm <combbm@naver.com>
2021-01-02 15:43:54 +0900

    fix: 학사일정 요일 오류 수정

--- src/services/school/calendar-service.ts
+++ src/services/school/calendar-service.ts
@@ -2,13 +2,12 @@ import dayjs from 'dayjs';
 import { range } from 'fxjs';
 import school from '../../utils/school-lib';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getThisWeekCalendar = async (): Promise<string[]> => {
   const calender: any = await school.getCalendar();
 
   const result = range(0, 5)
     .map((v: string, i: number) => {
-      const thisWeek = dayjs().date() + (i - dayjs().day());
+      const thisWeek = dayjs().date() + ((i + 1) - dayjs().day());
       const thisDay = dayjs()
         .set('date', thisWeek)
         .date();
