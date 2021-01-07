9267aa7be73d3b4632a5bf549063ef502189464b
combbm <combbm@naver.com>
2021-01-07 17:51:25 +0900

    fix: add logger

--- src/routers/notice-router.ts
+++ src/routers/notice-router.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuthenticated, requirePermission } from '../middlewares/permission';
 import ServerConfigs from '../databases/models/server-configs';
 import { checkValidation } from '../middlewares/validator';
+import { logger } from '../index';
 
 const router = express.Router();
 
@@ -23,29 +24,39 @@ router.get('/', requireAuthenticated, async (req, res) => {
     data: notice.configValue
   });
 });
+
 const noticeValidator = [
   body('notice').isString()
 ];
-router.put('/', requireAuthenticated, requirePermission(['admin', 'teacher']), noticeValidator, checkValidation, async (req: express.Request, res: express.Response) => {
-  const { notice: noticeValue } = req.body;
-  const notice = await ServerConfigs.findOne({
-    where: {
-      configKey: 'notice'
+router.put('/',
+  requireAuthenticated,
+  requirePermission(['admin', 'teacher']),
+  noticeValidator,
+  checkValidation,
+  async (req: express.Request, res: express.Response) => {
+    const { notice: noticeValue } = req.body;
+
+    const notice = await ServerConfigs.findOne({
+      where: {
+        configKey: 'notice'
+      }
+    });
+
+    if (!notice) {
+      res.status(404).json({ success: false });
+      return;
     }
-  });
 
-  if (!notice) {
-    res.status(404).json({ success: false });
-    return;
-  }
-  await notice.update({
-    configValue: noticeValue
-  });
+    await notice.update({
+      configValue: noticeValue
+    });
 
-  res.status(200).json({
-    success: true,
-    data: notice.configValue
+    res.status(200).json({
+      success: true,
+      data: notice.configValue
+    });
+
+    logger.info('공지시항 내용을 변경하였습니다.');
   });
-});
 
 export default router;
