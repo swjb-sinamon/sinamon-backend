0dec51f0c2f3ab2e406b76ce60e7819d931547e1
김도현 <73230323+Cdohyeon@users.noreply.github.com>
2021-01-07 17:38:50 +0900

    feat: 공지사항수정한API제작

--- src/routers/notice-router.ts
+++ src/routers/notice-router.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-import { requireAuthenticated } from '../middlewares/permission';
+import { body } from 'express-validator';
+import { requireAuthenticated, requirePermission } from '../middlewares/permission';
 import ServerConfigs from '../databases/models/server-configs';
+import { checkValidation } from '../middlewares/validator';
 
 const router = express.Router();
 
@@ -15,6 +17,30 @@ router.get('/', requireAuthenticated, async (req, res) => {
     res.status(404).json({ success: false });
     return;
   }
+
+  res.status(200).json({
+    success: true,
+    data: notice.configValue
+  });
+});
+const noticeValidator = [
+  body('notice').isString()
+];
+router.put('/', requireAuthenticated, requirePermission(['admin', 'teacher']), noticeValidator, checkValidation, async (req: express.Request, res: express.Response) => {
+  const { notice: noticeValue } = req.body;
+  const notice = await ServerConfigs.findOne({
+    where: {
+      configKey: 'notice'
+    }
+  });
+
+  if (!notice) {
+    res.status(404).json({ success: false });
+    return;
+  }
+  notice.update({
+    configValue: noticeValue
+  });
 
   res.status(200).json({
     success: true,
