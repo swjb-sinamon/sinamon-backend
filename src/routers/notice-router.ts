2805be342fc1f248932ba325f599e20408b65a4f
combbm <combbm@naver.com>
2021-01-07 17:52:38 +0900

    chore: add apidoc into notice router

--- src/routers/notice-router.ts
+++ src/routers/notice-router.ts
@@ -7,6 +7,17 @@ import { logger } from '../index';
 
 const router = express.Router();
 
+/**
+ * @api {get} /notice 공지사항 가져오기
+ * @apiName GetNotice
+ * @apiGroup Notice
+ *
+ * @apiSuccess {Boolean} success 성공 여부
+ * @apiSuccess {Object} data 데이터
+ *
+ * @apiError (Error 401) NO_PERMISSION 권한이 없습니다.
+ * @apiError (Error 500) SERVER_ERROR 오류가 발생하였습니다. 잠시후 다시 시도해주세요.
+ */
 router.get('/', requireAuthenticated, async (req, res) => {
   const notice = await ServerConfigs.findOne({
     where: {
@@ -28,6 +39,17 @@ router.get('/', requireAuthenticated, async (req, res) => {
 const noticeValidator = [
   body('notice').isString()
 ];
+/**
+ * @api {put} /notice 공지사항 수정
+ * @apiName UpdateNotice
+ * @apiGroup Notice
+ *
+ * @apiSuccess {Boolean} success 성공 여부
+ * @apiSuccess {Object} data 데이터
+ *
+ * @apiError (Error 401) NO_PERMISSION 권한이 없습니다.
+ * @apiError (Error 500) SERVER_ERROR 오류가 발생하였습니다. 잠시후 다시 시도해주세요.
+ */
 router.put('/',
   requireAuthenticated,
   requirePermission(['admin', 'teacher']),
