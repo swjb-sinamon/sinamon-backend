3676c6c23cb58ec9839ccde4549b56c3106988a2
combbm <combbm@naver.com>
2021-03-08 12:40:35 +0900

    feat: implement getFCMTokens() function

--- src/services/fcm-service.ts
+++ src/services/fcm-service.ts
@@ -1,6 +1,42 @@
+import { Op } from 'sequelize';
 import FCM from '../databases/models/fcm';
 import ServiceException from '../exceptions';
 import ErrorMessage from '../error/error-message';
+import { filter, FilterParam, pagination } from '../utils/router-util';
+import Users from '../databases/models/users';
+import { PaginationResult } from '../types/pagination-result';
+
+export const getFCMTokens = async (
+  offset?: number,
+  limit?: number,
+  nameSearchQuery?: string
+): Promise<PaginationResult<Array<FCM & { user: Users }>>> => {
+  const pageOption = offset && limit ? pagination(offset, limit) : {};
+
+  const filtering: FilterParam<never> = [];
+  if (nameSearchQuery) {
+    filtering.push([Op.like, 'name', `%${nameSearchQuery}%`]);
+  }
+
+  const { count, rows } = await FCM.findAndCountAll({
+    ...pageOption,
+    include: [
+      {
+        model: Users,
+        attributes: ['name', 'department', 'studentGrade', 'studentClass', 'studentNumber'],
+        as: 'user',
+        where: {
+          ...filter(filtering)
+        }
+      }
+    ] as never
+  });
+
+  return {
+    count,
+    data: rows as Array<FCM & { user: Users }>
+  };
+};
 
 export const getFCMToken = async (uuid: string): Promise<FCM> => {
   const current = await FCM.findOne({
