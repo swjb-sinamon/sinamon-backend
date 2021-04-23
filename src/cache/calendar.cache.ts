7557e25b45e4dd906f57a2343e0f62a3d0263ba3
김도현 <73230323+Cdohyeon@users.noreply.github.com>
2021-04-23 22:13:42 +0900

    fix: AcademicSchedule

--- src/cache/calendar.cache.ts
+++ src/cache/calendar.cache.ts
@@ -1,13 +1,31 @@
+import dayjs from 'dayjs';
 import { ApiCache } from './api-cache';
 import school from '../utils/school-lib';
 import { redisUtil } from '../index';
 
+let i = 0;
+const thisWeeks = dayjs().date() + (i + 1 - dayjs().day());
+const MonthDate = [31,28,31,30,31,30,31,30,31,31,30,31];
 class CalendarCache implements ApiCache<any> {
   readonly cacheKey: string = 'calendar';
 
   async fetchCache(): Promise<void> {
-    const data = await school.getCalendar();
-    await redisUtil.setAsync(this.cacheKey, JSON.stringify(data));
+    
+    if(thisWeeks>MonthDate[dayjs().month()]){
+      const data = await school.getCalendar(dayjs().year(),dayjs().month()+2);
+        await redisUtil.setAsync(this.cacheKey, JSON.stringify(data));
+        // eslint-disable-next-line no-plusplus
+        i++;
+    }else{
+      const data = await school.getCalendar(dayjs().year(),dayjs().month()+1);
+      await redisUtil.setAsync(this.cacheKey, JSON.stringify(data));
+      // eslint-disable-next-line no-plusplus
+      i++;
+    }
+    if(i===6){
+      i=0;
+    }
+    
   }
 
   async getCacheData(): Promise<any> {
--- src/services/school/calendar-service.ts
+++ src/services/school/calendar-service.ts
@@ -2,13 +2,21 @@ import dayjs from 'dayjs';
 import { range } from 'fxjs';
 import { CalendarCache } from '../../cache';
 
+const MonthDate = [31,28,31,30,31,30,31,30,31,31,30,31];
 export const getThisWeekCalendar = async (): Promise<string[]> => {
   const calender = await CalendarCache.getCacheData();
 
   const result = range(0, 5).map((v: string, i: number) => {
-    const thisWeek = dayjs().date() + (i + 1 - dayjs().day());
-    const thisDay = dayjs().set('date', thisWeek).date();
-    return calender[thisDay];
+    const thisWeeks = dayjs().date() + (i + 1 - dayjs().day());
+    if(MonthDate[dayjs().month()]<thisWeeks){
+      const thisWeek =  i + 1 - dayjs().day();
+      const thisDay = dayjs().set('date', thisWeek).date();
+      return calender[thisDay];
+    }
+      const thisWeek = dayjs().date() + (i + 1 - dayjs().day());
+      const thisDay = dayjs().set('date', thisWeek).date();
+      return calender[thisDay];
+  
   });
 
   return result;
