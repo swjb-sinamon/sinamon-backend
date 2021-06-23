8ae5d6d6b08c684342c523085549f3623d187e53
combbm <combbm@naver.com>
2021-06-23 10:22:35 +0900

    fix: 로직 정리

--- src/services/school/calendar-service.ts
+++ src/services/school/calendar-service.ts
@@ -6,18 +6,18 @@ import { getDateByWeekday, MONTH_DATE } from '../../utils/date-util';
 export const getThisWeekCalendar = async (): Promise<string[]> => {
   const calender = await CalendarCache.getCacheData();
 
-  const result = range(0, 5).map((value: number) => {
-    const thisWeeks = getDateByWeekday(value);
+  const result = range(0, 5).map((week: number) => {
+    const thisWeeks = getDateByWeekday(week);
 
-    if (MONTH_DATE[dayjs().month()] < thisWeeks) {
-      const thisWeek = value + 1 - dayjs().day();
+    // 다음달 학사일정
+    if (thisWeeks > MONTH_DATE[dayjs().month()]) {
+      const thisWeek = dayjs().date() + (week + 1 - dayjs().day());
       const thisDay = dayjs().set('date', thisWeek).date();
-      return calender.nowData[thisDay];
+      return calender.nextMonth[thisDay];
     }
 
-    const thisWeek = dayjs().date() + (value + 1 - dayjs().day());
-    const thisDay = dayjs().set('date', thisWeek).date();
-    return calender.nextData[thisDay];
+    // 이번달 학사일정
+    return calender.thisMonth[thisWeeks];
   });
 
   return result;
