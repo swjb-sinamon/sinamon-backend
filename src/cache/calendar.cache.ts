f99408960d6fcb4f5b7387ff1d6e81f5b3a70fb4
combbm <combbm@naver.com>
2021-06-23 10:13:15 +0900

    fix: change name of property in ReturnType

--- src/cache/calendar.cache.ts
+++ src/cache/calendar.cache.ts
@@ -4,20 +4,20 @@ import school from '../utils/school-lib';
 import { redisUtil } from '../index';
 
 interface ReturnData {
-  readonly nowData: any;
-  readonly nextData: any;
+  readonly thisMonth: any;
+  readonly nextMonth: any;
 }
 
 class CalendarCache implements ApiCache<ReturnData> {
   readonly cacheKey: string = 'calendar';
 
   async fetchCache(): Promise<void> {
-    const nextData = await school.getCalendar(dayjs().year(), dayjs().month() + 2);
-    const nowData = await school.getCalendar(dayjs().year(), dayjs().month() + 1);
+    const nextMonth = await school.getCalendar(dayjs().year(), dayjs().month() + 2);
+    const thisMonth = await school.getCalendar(dayjs().year(), dayjs().month() + 1);
 
     const data = {
-      nowData,
-      nextData
+      thisMonth,
+      nextMonth
     };
 
     await redisUtil.setAsync(this.cacheKey, JSON.stringify(data));
