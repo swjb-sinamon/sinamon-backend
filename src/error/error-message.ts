cdab8f58294d2f7c707316f19dccdabfdefd8182
DaegyeomHa <combbm@naver.com>
2021-08-23 18:15:55 +0900

    fix: delete unnecessary error message

--- src/error/error-message.ts
+++ src/error/error-message.ts
@@ -29,7 +29,6 @@ enum ErrorMessage {
   ANONYMOUS_CANNOT_DELETE = '답변은 자기자신만 삭제할 수 있습니다.',
 
   SUBJECT_NOT_FOUND = '존재하지 않는 과목입니다.',
-  SUBJECT_DATA_NOT_FOUND = '과목 데이터를 찾을 수 없습니다.',
   APPLICATION_ALREADY_EXISTS = '이미 과목 신청 하였습니다. 배정을 기다리거나 신청을 취소해주세요.',
   APPLICATION_NOT_FOUND = '과목 신청 기록을 찾을 수 없습니다.',
   INVAILD_APPLICATION = '올바른 배정 종류가 아닙니다.',
