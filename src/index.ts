af12d6ab1ec3ac92f50cebcb70329a7be899355e
DaegyeomHa <combbm@naver.com>
2021-08-25 14:35:15 +0900

    fix: API 버전, Swagger 주소 변경

--- src/index.ts
+++ src/index.ts
@@ -114,14 +114,14 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use('/v1/*', (req, res, next) => {
+app.use('/v2/*', (req, res, next) => {
   logger.info(`${req.ip} ${req.method} ${req.originalUrl} ${req.get('User-Agent')}`);
   next();
 });
-app.use('/v1', Router);
+app.use('/v2', Router);
 
 const specs = swaggerJSDoc(swaggerConfig);
-app.use('/', swaggerUI.serve, swaggerUI.setup(specs, { explorer: true }));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(specs, { explorer: true }));
 
 cron();
 
