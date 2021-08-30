1271f2b78de2bb7358a877ec7b644cca4560494a
DaegyeomHa <combbm@naver.com>
2021-08-30 20:29:57 +0900

    fix: 날짜 오류 수정

--- src/utils/date-util.ts
+++ src/utils/date-util.ts
@@ -5,4 +5,4 @@ type WeekIndex = 0 | 1 | 2 | 3 | 4; // 월, 화, 수, 목, 금
 export const getDateByWeekday = (weekIndex: WeekIndex | number) =>
   dayjs().date() + (weekIndex + 1 - dayjs().day());
 
-export const MONTH_DATE = [31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31];
+export const MONTH_DATE = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
